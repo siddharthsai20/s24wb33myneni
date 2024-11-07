9cc9254efbf6498498bf3abb5d816cfe6ff02aed
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-11-06 21:02:14 -0600

    everything is done

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gridRouter = require('./routes/grid');
+var indexRouter = require('./routes/index');
+var conferenceRouter = require('./routes/conference');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -21,6 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/index', indexRouter);
+app.use('/conference', conferenceRouter);
+app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
