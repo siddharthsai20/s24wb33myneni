bfbbd14075e5fa45ed74e5f47cfe278e55073012
siddharthsai20 <113754786+siddharthsai20@users.noreply.github.com>
2024-11-06 20:21:59 -0600

    new files added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var indexRouter = require('./routes/index');
+var conferenceRouter = require('./routes/conference');
 
 var app = express();
 
