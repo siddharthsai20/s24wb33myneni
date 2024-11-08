f191a5108df63aec823606133371150d7292c5ea
siddharth sai myneni <siddharthsai.myneni@gmail.com>
2024-11-07 23:52:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
  
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/conference', conferenceRouter);
 app.use('/grid', gridRouter);
